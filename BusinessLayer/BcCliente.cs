967d4dbd1c6ccdf78caec5c2303c6e61a9ae915e
nicoLeporati <n.leporati@alumnos.duoc.cl>
2018-04-11 00:21:59 -0300

    final 3

--- BusinessLayer/BcCliente.cs
+++ BusinessLayer/BcCliente.cs
@@ -10,7 +10,7 @@ namespace BusinessLayer
 {
     public class BcCliente
     {
-        /* Probar
+        /* 
          * Buscar un cliente por su rut, solo si el rut no es nulo y no esta en blanco
          */
         public Cliente BuscarPorRut(string rut = null)
